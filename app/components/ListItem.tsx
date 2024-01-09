35814d9d5886325aa4ff16a7ddc6772848f4ccbb
marykasp <kasparian.mary@gmail.com>
2024-01-09 14:22:07 -0600

    chore: fix posts and lisitem component to use Meta type

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import getFormattedDate from "../../lib/getFormattedDate";
 
 type Props = {
-  post: BlogPost;
+  post: Meta;
 };
 
 const ListItem = ({ post }: Props) => {
--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
-import { getSortedPostsData } from "../../lib/posts";
+import { getPostsMeta } from "../../lib/posts";
 import ListItem from "./ListItem";
 
-const Posts = () => {
-  const posts = getSortedPostsData();
+const Posts = async () => {
+  // get meta data from all the posts
+  const posts = await getPostsMeta();
   console.log(posts);
+
+  if (!posts) {
+    return <p className="mt-10 text-center">Sorry, no posts available</p>;
+  }
 
   return (
     <section className="mt-10  max-w-2xl text-white">
       <h2 className="text-3xl font-semibold border-b pb-2 border-zinc-800 dark:text-white/90">
         Latest Posts
       </h2>
-      <ul className="w-full mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+      <ul className="w-full mt-4 grid grid-cols-1 md:grid-cols-2 gap-4 list-none p-0">
         {/* display only 4 most recent posts */}
         {posts.slice(0, 4).map((post) => (
           <ListItem post={post} key={post.id} />
