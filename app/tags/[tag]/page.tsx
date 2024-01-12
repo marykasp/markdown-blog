ba8888338a894a1f761fdc0954c086a6944b8a9b
marykasp <kasparian.mary@gmail.com>
2024-01-12 13:48:22 -0600

    feat: create a function to get the tags from the meta data, remove duplicates and return an object of tags

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -2,3 +2,24 @@
 import { getPostsMeta } from "../../../lib/posts";
 import ListItem from "../../components/ListItem";
 import Link from "next/link";
+
+export const revalidate = 0;
+
+type Props = {
+  params: {
+    tag: string;
+  };
+};
+
+// export async function generateStaticParams() {
+//   // get post meta data - returns array of Meta objects
+//   const posts = await getPostsMeta();
+
+//   if (!posts) return [];
+
+//   // map over all the posts and get the tags - [[]] - flatten array of array - create a set to remove duplicates
+//   const tags = new Set(posts.map((post) => post.tags).flat());
+
+//   // need an array of objects - map over array of tags - create object (key:value)
+//   return Array.from(tags).map((tag) => ({ tag }));
+// }
