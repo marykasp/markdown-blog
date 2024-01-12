d2418b9a7cf88d0c927efe3968ea24ae8d2addd5
marykasp <kasparian.mary@gmail.com>
2024-01-12 16:56:55 -0600

    style: quick styling of blog page that lists all the posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,23 @@
-const Blog = () => {
-  return <div>List of blog posts</div>;
+import { getPostsMeta } from "../../lib/posts";
+import ListItem from "../components/ListItem";
+
+const Blog = async () => {
+  const posts = await getPostsMeta();
+
+  if (!posts) return <p>Sorry no posts yet on this blog</p>;
+
+  return (
+    <>
+      <h2 className="text-white font-bold text-2xl my-6">List of All Posts</h2>
+      <section className="w-full max-w-4xl">
+        <ul className="w-full flex flex-col gap-4">
+          {posts.map((post) => (
+            <ListItem key={post.id} post={post} />
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 };
 
 export default Blog;
